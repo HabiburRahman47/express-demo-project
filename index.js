93119156c55a674d79c1e81e9ff6181f6aed0f1a
Habibur Rahman <habiburkuetacbd@gmail.com>
2024-08-02 15:40:29 +0600

    worked on post routing with body parameter from postman

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
@@ -9,9 +10,20 @@ const logger = (req, res, next) =>{
 }
 app.use(logger);
 
-app.use(express.json());
+//parse URL-endcoded bodies
+app.use(bodyParser.urlencoded({extended: false}));
+
+//parse JSON bodies
+app.use(bodyParser.json());
 app.get('/',(req, res) => {
     res.send('Hello, world!');
+
+});
+
+app.post('/user', (req, res) =>{
+    const name= req.body.name;
+    const age = req.body.age;
+    res.send(`User name is ${name} and age is ${age}`)
 });
 
 app.listen(port, () =>{
